01c79dd9de43d7eb37e8c832ecfd177888eadb05
Kevin Nwoke <kevinnwoke@yahoo.com>
2023-03-13 23:22:56 -0500

    Add files via upload

---
+++ CarListing.py
@@ -0,0 +1,31 @@
+from bs4 import BeautifulSoup
+import requests
+import pandas as pd
+
+headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/105.0.0.0 Safari/537.36'}
+
+carList = []
+
+def myCars(page):
+    url = f'https://www.cars.com/shopping/results/?page={page}&page_size=100&list_price_max=&makes[]=&maximum_distance=100&models[]=&stock_type=all&zip=75287'    
+    r = requests.get(url, headers=headers)
+    soup = BeautifulSoup(r.text, 'html.parser')
+    cars = soup.find_all('div', {'class': 'vehicle-card'})
+    for item in cars:
+        car = {    
+        'monthly': item.find('span', {'class': 'js-estimated-monthly-payment-formatted-value-with-abr'}),    
+        'title': item.find('h2', {'class': 'title'}).text,
+        'price': item.find('span', {'class': 'primary-price'}).text,
+        'miles': item.find('div', {'class': 'miles-from'}),
+        'mileage': item.find('div', {'class': 'mileage'}),
+        'oneOwner': item.find('a', {'class': 'sds-link--ext'})
+        }
+        carList.append(car)
+    return carList
+
+for x in range(196,210):
+    myCars(x)
+    
+df = pd.DataFrame(carList)   
+df.to_csv('carsList16.csv', index=False)
+
